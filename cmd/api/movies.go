bf005e45ae21c5f71a24aab2ce2ec9e104ea2677
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-11-12 18:00:12 +0100

    refactor: :building_construction: readIDParam as helper function

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // Add createMovieHandler for "POST /v1/movies" endpoint
@@ -15,12 +12,8 @@ func (app *application) createMovieHandler(rw http.ResponseWriter, r *http.Reque
 
 // Add showMovieHandler for "GET /v1/movies/:id" endpoint + retrieve interpolated "id" parameter from current URL
 func (app *application) showMovieHandler(rw http.ResponseWriter, r *http.Request) {
-	// Store any interpolated URL params in request context during parsing, retrieve a slice containing params names & values
-	params := httprouter.ParamsFromContext(r.Context())
-
-	// Get value of "id" parameter from slice, convert returned string to base 10 int (with bitsize of 64)
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(rw, r)
 		return
 	}
