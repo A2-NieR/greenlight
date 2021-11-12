f3e0f950932bc4ec758d1264e770d41ba2c35915
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-11-12 17:55:07 +0100

    feat: :sparkles: add router, prep movie routers & handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -41,14 +41,10 @@ func main() {
 		logger: logger,
 	}
 
-	// Declare a new servemux and add a /v1/healthcheck route that dispatches requests to the healthcheckHandler method
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	// Declare a HTTP server with timeout settings that listens on provided port in config struct and uses servemux from above as handler
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
