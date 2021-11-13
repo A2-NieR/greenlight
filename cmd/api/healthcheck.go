94f0624fd369ef0015543638b0f4878e9af135dd
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-11-13 12:30:37 +0100

    feat: :sparkles: writejson helper

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-// Plain text response handler w/ info about application status, operating environment & version
 func (app *application) healthcheckHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "status: available")
-	fmt.Fprintf(rw, "environment: %s\n", app.config.env)
-	fmt.Fprintf(rw, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(rw, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(rw, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
