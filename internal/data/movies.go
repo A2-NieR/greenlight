158fa10cb340071b587b11e135efc00ea1768e88
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-11-17 19:10:32 +0100

    fix: :bug: multiple delete reqs on same id

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -131,9 +131,9 @@ func (m MovieModel) Delete(id string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	_, err = m.Collection.DeleteOne(ctx, delete)
-	if err != nil {
-		return err
+	res, _ := m.Collection.DeleteOne(ctx, delete)
+	if res.DeletedCount == 0 {
+		return mongo.ErrNoDocuments
 	}
 
 	return nil
